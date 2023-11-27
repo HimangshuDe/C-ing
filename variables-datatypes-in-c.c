633e6763e8b7be930241e50e494673fcdedcd55d
DeX-Coder <dehimangshu03@gmail.com>
2023-11-27 09:47:59 +0530

    Variables and Data Types in C

---
+++ variables-datatypes-in-c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    int a_variable = 1;
+    char another_variable = 'A';
+    char string_variable[] = "Hello\n";
+    int random_number = 2145;
+
+    printf(string_variable, "\n");
+    printf("%d\n", a_variable);
+    printf("%d\n", random_number);
+    printf("%d\n", 14 + 56);
+}
