e6eab87acc68ff889654d1f3668629a7308d15ee
Himangshu De <120447873+HimangshuDe@users.noreply.github.com>
2024-01-10 20:59:09 +0530

    String.h Unpacked(Formatted Edition)
    Added some formattings and uncommented important lines of code.

--- all-str-functions.c
+++ all-str-functions.c
@@ -6,28 +6,59 @@ void ustrcopy(char source[], char dest[]);
 
 int ustrcompare(char string1[], char string2[]);
 
-int ustringlen(char string[]);
+int ustrlen(char string[]);
 
-void ustringlower(char string[]);
+void ustrlower(char string[]);
+
+void ustrreverse(char string[]);
 
 // Entry point of a C program.
 int main()
 {
-    // char a[] = "AbC", b[]="AbC";
-    // ustrcopy(a, b);
+    char source1[] = "AbC", dest1[]="";
 
-    // char a[] = "cat", b[]="zebra";
-    // printf("%d",strcmp(a, b)); 
-    // printf("%d", ustrcompare(a, b));
+    ustrcopy(source1, dest1);
+    printf("Using custom function of strcpy():\n");
+    printf("---------------------------\n");
+    printf("Value of A:%s\nValue of B:%s\n", source1, dest1);
 
-    // char a[] = "cat";
-    // printf("%d\n", strlen(a));
-    // printf("%d\n", ustringlen(a));
+    printf("\n");
 
-    char a[100] = "CAT", b[100]="APple";
-    printf("%s\n",strlwr(a));
-    ustringlower(b);
-    printf("%s", b);
+    char str[] = "cat", str2[] ="zebra";
+    printf("strcmp() vs ustrcompare():\n");
+    printf("---------------------------\n");
+    printf("Comparison(strcmp): %d\n",strcmp(str, str2)); 
+    printf("Comparison(custom): %d\n", ustrcompare(str, str2));
+
+    printf("\n");
+
+    char word[] = "cat";
+    printf("strlen() vs ustrlen():\n");
+    printf("---------------------------\n");
+    printf("Length(strlen):%d\n", strlen(word));
+    printf("Length(custom):%d\n", ustrlen(word));
+    
+    printf("\n");
+
+    char new_word[100] = "tIgER";
+    printf("strlwr() vs ustrlower():\n");
+    printf("---------------------------\n");
+    printf("Word:%s\n", new_word);
+    printf("Using in built strlwr: %s\n",strlwr(new_word));
+    ustrlower(new_word);
+    printf("Lowered using custom func: %s\n",new_word);
+
+    printf("\n");
+
+    char a[100] = "APple";
+    
+    printf("strrev() vs ustrreverse():\n");
+    printf("---------------------------\n");
+    printf("Original text: %s\n", a);
+    printf("Reversed(strrev): %s\n", strrev(a));
+    ustrreverse(a);
+    printf("Reversed(custom): %s\n", a);
+
     return 0;
 }
 
@@ -54,7 +85,7 @@ int ustrcompare(char string1[], char string2[])
     return comp_result;
 }
 
-int ustringlen(char string[])
+int ustrlen(char string[])
 {
     int count = 0, i = 0;
     while (string[i] != '\0')
@@ -65,7 +96,7 @@ int ustringlen(char string[])
     return count;
 }
 
-void ustringlower(char string[]){
+void ustrlower(char string[]){
     int i =0;
     while (string[i] != '\0'){
         if(string[i] >= 'A' && string[i] <= 'Z'){
@@ -73,4 +104,21 @@ void ustringlower(char string[]){
         }
         i++;
     }
+}
+
+void ustrreverse(char string[]){
+    int i=0, count = ustrlen(string);
+    char temp_str[100];
+    i=0;
+    for (int j=count-1; j>=0; j--){
+        temp_str[i] = string[j];
+        i++;
+    }
+    i = 0;
+    while(i<=count){
+        // printf("In while");
+        string[i] = temp_str[i];
+        i++;
+    }
+
 }
